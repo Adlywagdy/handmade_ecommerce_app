634ca4d1737029bb18c3edcb62b5bdd872f617a4
Adly Wagdy Adly <adlywagdy666@gmail.com>
2026-03-23 16:17:50 +0200

    Enhance cart item controls and cart button customization

--- lib/core/widgets/customiconbutton.dart
+++ lib/core/widgets/customiconbutton.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class CustomIconButton extends StatelessWidget {
   final Color? backgroundColor;
   final IconData? icon;
+  final double? iconsize;
   final Color? iconcolor;
   const CustomIconButton({
     super.key,
     required this.backgroundColor,
     required this.icon,
     required this.iconcolor,
+    this.iconsize = 25,
   });
 
   @override
@@ -19,6 +22,7 @@ class CustomIconButton extends StatelessWidget {
 
       style: ButtonStyle(
         backgroundColor: WidgetStatePropertyAll(backgroundColor),
+        iconSize: WidgetStatePropertyAll(iconsize!.sp),
       ),
       icon: Icon(icon, color: iconcolor),
     );
--- lib/features/customer/presentation/widgets/amountcontainerbutton.dart
+++ lib/features/customer/presentation/widgets/amountcontainerbutton.dart
@@ -6,20 +6,31 @@ class AmountContainerButton extends StatelessWidget {
   final double? fontSize;
   final Color? iconscolor;
   final Color? textcolor;
+  final double circularradius;
+  final double verticalpadding;
+  final double horizontalpadding;
+  final double? spacingwidth;
   const AmountContainerButton({
     super.key,
     this.fontSize = 16,
     this.iconscolor = commonColor,
     this.textcolor = blackDegree,
+    this.circularradius = 12,
+    this.verticalpadding = 12,
+    this.horizontalpadding = 12,
+    this.spacingwidth = 12,
   });
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.symmetric(vertical: 12.h, horizontal: 12.w),
+      padding: EdgeInsets.symmetric(
+        vertical: verticalpadding.h,
+        horizontal: horizontalpadding.w,
+      ),
       decoration: BoxDecoration(
         color: commonColor.withValues(alpha: .05),
-        borderRadius: BorderRadius.circular(12.r),
+        borderRadius: BorderRadius.circular(circularradius.r),
         border: Border.all(
           color: commonColor.withValues(alpha: .1),
           width: 1.5,
@@ -27,12 +38,14 @@ class AmountContainerButton extends StatelessWidget {
       ),
 
       child: Row(
+        spacing: spacingwidth!.w,
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           InkWell(
             onTap: () {},
             child: Icon(Icons.remove, color: iconscolor),
           ),
+
           Text(
             '1', // This should be a variable that holds the current amount selected by the user
             textAlign: TextAlign.center,
@@ -44,6 +57,7 @@ class AmountContainerButton extends StatelessWidget {
               height: 1.50,
             ),
           ),
+
           InkWell(
             onTap: () {},
             child: Icon(Icons.add, color: iconscolor),
