eb412fe41bdea348433ab657cd92dee884306b31
Adly Wagdy Adly <adlywagdy666@gmail.com>
2026-03-30 23:26:43 +0200

    Integrates new UI and navigation into application entrypoint

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:handmade_ecommerce_app/features/customer/models/customer_model.dart';
-
 import 'package:handmade_ecommerce_app/features/customer/models/data/test_productslistdata.dart';
 import 'package:handmade_ecommerce_app/features/customer/models/order_model.dart';
-import 'package:handmade_ecommerce_app/features/customer/models/payment_model.dart';
-import 'package:handmade_ecommerce_app/features/customer/presentation/screens/customer_cart_screen.dart';
-
+import 'package:handmade_ecommerce_app/features/customer/presentation/screens/customer_orderdetails_screen.dart';
+import 'package:handmade_ecommerce_app/features/customer/presentation/screens/customer_writereview_screen.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 void main() async {
@@ -29,19 +27,115 @@ class HandcraftedEcommerceApp extends StatelessWidget {
         return GetMaterialApp(
           debugShowCheckedModeBanner: false,
 
-          home: CustomerCartScreen(
-            order: OrderModel(
-              customer: CustomerModel(name: "Adly"),
-              products: productsListData,
-              orderid: "123",
-              payment: PaymentDetailsModel(
-                subtotalPrice: 500,
-                totalPrice: 520,
-                deliveryFee: 20,
-                discount: 0,
+          home:
+              //CustomerWriteReviewScreen(product: productsListData[0]),
+              CustomerOrderDetailsScreen(
+                order: OrderModel(
+                  customer: CustomerModel(name: "adly"),
+                  products: productsListData,
+                  orderid: '#AY-9402',
+                ),
               ),
-            ),
-          ),
+
+          // CustomerProfilesScreen(
+          //   customer: CustomerModel(
+          //     name: "Adly Wagdy",
+          //     email: "adly.wagdy@ayady.com",
+          //     image: "assets/images/splash.jpeg",
+          //   ),
+          // ),
+          //  CustomerOrdersScreen(
+          //   customerorderslist: [
+          //     OrderModel(
+          //       customer: CustomerModel(name: "adly"),
+          //       orderid: '#AY-84920',
+          //       products: productsListData,
+
+          //       payment: PaymentDetailsModel(
+          //         paymentMethod: 'Credit Card',
+          //         totalPrice: 500.00,
+          //         discount: 50.00,
+          //       ),
+          //       orderDate: DateTime.now().subtract(const Duration(days: 2)),
+          //       status: .delivered,
+          //     ),
+          //     OrderModel(
+          //       customer: CustomerModel(name: "adly"),
+          //       orderid: '#AY-84920',
+          //       products: productsListData,
+
+          //       payment: PaymentDetailsModel(
+          //         paymentMethod: 'Credit Card',
+          //         totalPrice: 500.00,
+          //         discount: 50.00,
+          //       ),
+          //       orderDate: DateTime.now().subtract(const Duration(days: 2)),
+          //       status: .confirmed,
+          //     ),
+          //     OrderModel(
+          //       customer: CustomerModel(name: "adly"),
+          //       orderid: '#AY-84920',
+          //       products: productsListData,
+
+          //       payment: PaymentDetailsModel(
+          //         paymentMethod: 'Credit Card',
+          //         totalPrice: 500.00,
+          //         discount: 50.00,
+          //       ),
+          //       orderDate: DateTime.now().subtract(const Duration(days: 2)),
+          //       status: .pending,
+          //     ),
+          //     OrderModel(
+          //       customer: CustomerModel(name: "adly"),
+          //       orderid: '#AY-84920',
+          //       products: productsListData,
+
+          //       payment: PaymentDetailsModel(
+          //         paymentMethod: 'Credit Card',
+          //         totalPrice: 500.00,
+          //         discount: 50.00,
+          //       ),
+          //       orderDate: DateTime.now().subtract(const Duration(days: 2)),
+          //       status: .shipped,
+          //     ),
+          //     OrderModel(
+          //       customer: CustomerModel(name: "adly"),
+          //       orderid: '#AY-84920',
+          //       products: productsListData,
+          //       payment: PaymentDetailsModel(
+          //         paymentMethod: 'PayPal',
+          //         totalPrice: 124.00,
+          //         discount: 20.00,
+          //       ),
+          //       orderDate: DateTime.now().subtract(const Duration(days: 1)),
+          //       status: .cancelled,
+          //     ),
+          //     OrderModel(
+          //       customer: CustomerModel(name: "adly"),
+          //       orderid: '#AY-84920',
+          //       products: productsListData,
+          //       payment: PaymentDetailsModel(
+          //         paymentMethod: 'PayPal',
+          //         totalPrice: 124.00,
+          //         discount: 20.00,
+          //       ),
+          //       orderDate: DateTime.now().subtract(const Duration(days: 1)),
+          //       status: .returned,
+          //     ),
+          //     OrderModel(
+          //       customer: CustomerModel(name: "adly"),
+          //       orderid: '#AY-84920',
+          //       products: productsListData,
+          //       payment: PaymentDetailsModel(
+          //         paymentMethod: 'PayPal',
+          //         totalPrice: 124.00,
+          //         discount: 20.00,
+          //       ),
+          //       orderDate: DateTime.now().subtract(const Duration(days: 1)),
+          //       status: .preparing,
+          //     ),
+          //   ],
+          // ),
         );
       },
     );
