f2c35d17c76da64826ef0b9a81097e2bd6a94eae
Adly Wagdy Adly <adlywagdy666@gmail.com>
2026-03-30 23:26:43 +0200

    Enhances order tracking and navigation features

--- lib/features/customer/presentation/screens/customer_orders_screen.dart
+++ lib/features/customer/presentation/screens/customer_orders_screen.dart
@@ -1,17 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:handmade_ecommerce_app/core/theme/colors.dart';
+import 'package:handmade_ecommerce_app/core/widgets/customiconbutton.dart';
+import 'package:handmade_ecommerce_app/features/customer/models/order_model.dart';
+import 'package:handmade_ecommerce_app/features/customer/presentation/widgets/orderitem.dart';
 
 class CustomerOrdersScreen extends StatelessWidget {
-  const CustomerOrdersScreen({super.key});
+  final List<OrderModel> customerorderslist;
+  const CustomerOrdersScreen({super.key, required this.customerorderslist});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: customerbackGroundColor,
       appBar: AppBar(
+        backgroundColor: customerbackGroundColor,
+        scrolledUnderElevation: 0,
+        centerTitle: true,
+        actions: [
+          CustomIconButton(
+            backgroundColor: customerbackGroundColor,
+            icon: Icons.search,
+            iconcolor: commonColor,
+          ),
+        ],
+
         title: Text(
           'My Orders',
           style: TextStyle(
-            color: const Color(0xFF0F172A),
+            color: blackDegree,
             fontSize: 18.sp,
             fontFamily: 'Plus Jakarta Sans',
             fontWeight: FontWeight.w700,
@@ -19,6 +36,25 @@ class CustomerOrdersScreen extends StatelessWidget {
             letterSpacing: -0.45,
           ),
         ),
+      ),
+      // tab bar should be here to filter orders by status (all, delivered, canceled, etc.)
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: customerorderslist.length,
+              padding: const EdgeInsets.symmetric(horizontal: 16.0).w,
+              itemBuilder: (context, index) {
+                return OrderItem(order: customerorderslist[index]);
+              },
+            ),
+          ),
+          // Card(
+          //   color: Colors.white,
+          //   margin: EdgeInsets.zero,
+          //      child: CustomBottomBar(items: customerBottomBarItems),
+          // ),
+        ],
       ),
     );
   }
