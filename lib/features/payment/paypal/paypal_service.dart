8a74486f888916c7d7e35ccaf7766fcaa3d4be57
Adly Wagdy Adly <adlywagdy666@gmail.com>
2026-04-07 22:28:03 +0200

    init paypal package

---
+++ lib/features/payment/paypal/paypal_service.dart
@@ -0,0 +1,43 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_paypal_payment/flutter_paypal_payment.dart';
+import 'package:handmade_ecommerce_app/features/payment/paypal/paypal_models.dart';
+
+class PayPAlService {
+  void makePayPalpayment(
+    BuildContext context,
+    AmountPaymentModel amount,
+    ItemListModel orderItems,
+  ) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (BuildContext context) => PaypalCheckoutView(
+          sandboxMode: true, //testingmode
+          clientId: "",
+          secretKey: "",
+          transactions: [
+            {
+              "amount": amount.toJson(),
+              "description": "The payment transaction description.",
+              "item_list": orderItems.toJson(),
+            },
+          ],
+          note: "Contact us for any questions on your order.",
+          onSuccess: (Map params) async {
+            log("onSuccess: $params");
+            Navigator.pop(context);
+          },
+          onError: (error) {
+            log("onError: $error");
+            Navigator.pop(context);
+          },
+          onCancel: () {
+            log('cancelled:');
+            Navigator.pop(context);
+          },
+        ),
+      ),
+    );
+  }
+}
