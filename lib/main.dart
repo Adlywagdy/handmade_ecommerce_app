d0b59072ac1c63a194965b235c5cfb78a753c75d
DESKTOP-MKGK4HT\Mohammed <mohamedelkerba0@gmail.com>
2026-04-24 23:14:06 +0300

    no message

--- lib/main.dart
+++ lib/main.dart
@@ -5,36 +5,29 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
-import 'package:handmade_ecommerce_app/core/functions/get_initial_route.dart';
-import 'package:handmade_ecommerce_app/core/services/hivehelper_service.dart';
+import 'package:handmade_ecommerce_app/core/routes/routes.dart';
 import 'package:handmade_ecommerce_app/features/auth/cubit/auth_cubit.dart';
-import 'package:handmade_ecommerce_app/features/customer/cart/cart_cubit/cart_cubit.dart';
-import 'package:handmade_ecommerce_app/features/customer/home/cubit/home_cubit.dart';
-import 'package:handmade_ecommerce_app/features/customer/orders/cubit/order_cubit.dart';
-import 'package:handmade_ecommerce_app/features/customer/search/cubit/search_cubit.dart';
-import 'package:handmade_ecommerce_app/features/customer/profile/cubit/customer_cubit.dart';
-import 'package:handmade_ecommerce_app/features/customer/wishlist/cubit/wishlist_cubit.dart';
 import 'package:handmade_ecommerce_app/features/notifications/cubit/notifications_cubit.dart';
-import 'package:handmade_ecommerce_app/features/customer/reviews/cubit/reviews_cubit.dart';
 import 'package:handmade_ecommerce_app/features/seller/cubit/seller_cubit.dart';
 import 'package:handmade_ecommerce_app/features/seller/services/seller_firestore_service.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'core/routes/app_pages.dart';
 import 'core/services/remote_config_services.dart';
+import 'features/customer/cart/cart_cubit/cart_cubit.dart';
+import 'features/customer/home/cubit/home_cubit.dart';
+import 'features/customer/orders/cubit/order_cubit.dart';
+import 'features/customer/profile/cubit/customer_cubit.dart';
+import 'features/customer/search/cubit/search_cubit.dart';
+import 'features/customer/wishlist/cubit/wishlist_cubit.dart';
 import 'firebase_options.dart';
 import 'package:handmade_ecommerce_app/features/auth/services/auth_service.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
+  await Firebase.initializeApp();
   await Hive.initFlutter();
   await Hive.openBox('notifications');
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-
-  await Hive.openBox(HiveHelper.onboardingBox);
-  await Hive.openBox(HiveHelper.login);
-  await Hive.openBox(HiveHelper.email);
 
   // ─── TEST FIREBASE CONNECTION ───
   try {
@@ -50,23 +43,18 @@ void main() async {
 
   //////////////////////////// Crashlytics ///////////////////////////////////
   FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
-
   PlatformDispatcher.instance.onError = (error, stack) {
     FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
     return true;
   };
-
+  ///////////////////////////// RemoteConfig //////////////////////////////////
   await RemoteConfigService.instance.init();
-
-  final initialRoute = await getInitialRoute();
-
-  runApp(HandcraftedEcommerceApp(initialRoute: initialRoute));
+  /////////////////////////////////////////////////////////////////////////
+  runApp(const HandcraftedEcommerceApp());
 }
 
 class HandcraftedEcommerceApp extends StatelessWidget {
-  final String initialRoute;
-
-  const HandcraftedEcommerceApp({super.key, required this.initialRoute});
+  const HandcraftedEcommerceApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -81,12 +69,10 @@ class HandcraftedEcommerceApp extends StatelessWidget {
               create: (BuildContext context) => AuthCubit(AuthService()),
             ),
             BlocProvider(
-              create: (BuildContext context) =>
-                  SellerCubit(SellerFirestoreService())..loadDashboard(),
+              create: (BuildContext context) => SellerCubit(SellerFirestoreService())..loadDashboard(),
             ),
             BlocProvider(
-              create: (BuildContext context) =>
-                  NotificationsCubit()..loadNotifications(),
+              create: (BuildContext context) => NotificationsCubit()..loadNotifications(),
             ),
             BlocProvider(create: (BuildContext context) => CustomerCubit()),
             BlocProvider(
@@ -102,12 +88,10 @@ class HandcraftedEcommerceApp extends StatelessWidget {
             ),
             BlocProvider(create: (context) => CartCubit()..getcartProducts()),
             BlocProvider(create: (context) => OrderCubit()..getAllOrders()),
-            BlocProvider(create: (context) => ReviewsCubit()),
           ],
           child: GetMaterialApp(
             debugShowCheckedModeBanner: false,
-            initialRoute: initialRoute,
-
+            initialRoute: AppRoutes.onboarding,
             getPages: AppPages.pages,
           ),
         );
